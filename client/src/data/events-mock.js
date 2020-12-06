86c40d41a2866cc3ce8cfc58d2701662591bbb76
VasiliyMac <cartario@yandex.ru>
2020-12-06 15:45:51 +0300

    popup

--- client/src/data/events-mock.js
+++ client/src/data/events-mock.js
@@ -1,17 +1,22 @@
-
-import img from '../data/poster01.jpg';
-import img2 from '../data/poster02.jpg';
-import img3 from '../data/poster03.jpg';
+import img01 from '../img/events/1.jpg';
+import img02 from '../img/events/2.jpg';
+import img03 from '../img/events/3.jpg';
+import img04 from '../img/events/4.jpg';
+import img05 from '../img/events/5.jpg';
+import img06 from '../data/poster01.jpg';
+import img07 from '../data/poster02.jpg';
+import img08 from '../data/poster03.jpg';
 
 export const events = [
   {
     id: '1q',
     title: 'Соревнования по киберспорту, посвященные Дню конституции',
-    date: new Date(2020, 11, 11, 18, 0),
+    date: new Date(2019, 11, 11, 18, 0),
     place: 'Люсиновская, 53',
     description: 'description description description description description',
-    posterUrl: img,
+    posterUrl: img01,
     category: 'концерт',
+    photos: [img01, img02, img03],
   },
   {
     id: '2sq',
@@ -19,8 +24,9 @@ export const events = [
     date: new Date(2020, 11, 13, 19, 0),
     place: 'Люсиновская, 54',
     description: 'description description desc',
-    posterUrl: img2,
+    posterUrl: img02,
     category: 'акция',
+    photos: [img01, img02, img03],
   },
   {
     id: '2aq',
@@ -28,8 +34,9 @@ export const events = [
     date: new Date(2020, 11, 13, 19, 0),
     place: 'Люсиновская, 54',
     description: 'description description desc',
-    posterUrl: img2,
+    posterUrl: img03,
     category: 'акция',
+    photos: [img01, img02, img03],
   },
   {
     id: '3q',
@@ -39,6 +46,7 @@ export const events = [
     description: 'description description desc description description desc',
     posterUrl: null,
     category: 'кдн',
+    photos: [img01, img02, img03],
   },
   {
     id: '4q',
@@ -46,7 +54,8 @@ export const events = [
     date: new Date(2020, 11, 13, 19, 0),
     place: 'Люсиновская, 54',
     description: 'description description desc description description desc',
-    posterUrl: img3,
+    posterUrl: img04,
     category: 'концерт2',
+    photos: [img01, img02, img03],
   },
 ];
--- client/src/pages/events.jsx
+++ client/src/pages/events.jsx
@@ -2,87 +2,133 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import Header from '../components/header';
 import SoonEventsSlider from '../components/soon-events-slider';
-import {events} from '../data/events-mock';
-import img01 from '../img/events/1.jpg';
-import img02 from '../img/events/2.jpg';
-import img03 from '../img/events/3.jpg';
-import img04 from '../img/events/4.jpg';
-import img05 from '../img/events/5.jpg';
+import { events } from '../data/events-mock';
+import emptyImg from '../data/card01.jpg';
+
+const EventsByMonth = ({ events, title, setEvent, setVisible, visible, handleEventClick }) => {
+
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul className="events__list">
+        {events &&
+          events.map((event) => (
+            <li key={event.id} className="events__item" onClick={() => handleEventClick(event)}>
+              <img className="events__img" src={event.posterUrl || emptyImg} alt={event.title} />
+              {event.posterUrl ? '' : <span>{event.title.substr(0, 7)}...</span>}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
 
 const Events = () => {
   const [count, setCount] = React.useState(0);
+  const [visible, setVisible] = React.useState(false);
+  const [event, setEvent] = React.useState(null);
+  const popupRef = React.useRef();
+
+  const handleEventClick = (event) => {
+    setEvent(event);
+    setVisible(true);
+  };
+
+  const handleEscKeyDown = (e) => {
+    const isEsc = e.keyCode === 27;    
+    if(visible&&isEsc){
+      setVisible(false)
+    }
+  }
+
+  const handleClickOutPopup = (e) => {
+    const isOutside = !e.path.includes(popupRef.current);
+    if(visible&&isOutside){      
+      setVisible(false);
+    }
+  }
+
+  React.useEffect(()=>{
+    if(visible){
+      document.body.setAttribute('style', "overflow:hidden")
+    }
+    else{
+      document.body.setAttribute('style', "overflow:scroll")
+    }
+
+    document.addEventListener('keydown', handleEscKeyDown);
+    document.addEventListener('click', handleClickOutPopup);
+    
+    return function (){
+      document.removeEventListener('keydown', handleEscKeyDown);
+      document.removeEventListener('click', handleClickOutPopup);
+    }    
+  })
 
   return (
     <>
       <Navbar />
-      <Header title="Мероприятия"/>
+      <Header title="Мероприятия" />
       <main className="events">
         {/* <SoonEventsSlider {...{filteredEvents: events, count, setCount}}/> */}
 
-        <h2>Декабрь 2020</h2>
-        <ul className="events__list">
-          <li className="events__item">
-            <img className="events__img" src={img01} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img02} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img03} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img04} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img05} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img01} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img02} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img03} alt="img"/>
-          </li>         
-          </ul>
+        <EventsByMonth
+          title={'Декабрь 2020'}
+          events={events.slice(0, 3)}
+          setEvent={setEvent}
+          setVisible={setVisible}
+          visible={visible}
+          handleEventClick={handleEventClick}
+        />
+        <EventsByMonth
+          title={'Ноябрь 2020'}
+          events={events.slice(0, 6)}
+          setEvent={setEvent}
+          setVisible={setVisible}
+          visible={visible}
+          handleEventClick={handleEventClick}
+        />
+        <EventsByMonth
+          title={'Октябрь 2020'}
+          events={events.slice(2, 7)}
+          setEvent={setEvent}
+          setVisible={setVisible}
+          visible={visible}
+          handleEventClick={handleEventClick}
+        />
 
-          <h2>Ноябрь 2020</h2>
-        <ul className="events__list">
-          
-          <li className="events__item">
-            <img className="events__img" src={img04} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img05} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img01} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img02} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img03} alt="img"/>
-          </li>         
-          </ul>
+        {visible && (
+          <div className="popup">
+            <div ref= {popupRef} className="popup__inner">
+              <h4>{event && event.title}</h4>
+              <p>{event.description}</p>
+              <div className="events__popup-info">
+                
+                <span>{event.place} </span>
+                <span>{event.category} </span>
+                <span>{event.date.toLocaleString()} </span>
+              </div>
+              <img className="events__poster" src={event.posterUrl || emptyImg} alt="imgPoster" />              
 
+              {new Date() < event.date ? (
+                ''
+              ) : (
+                <div className="events__old">
+                  <h4>Как это было:</h4>
+                  <img src={event.photos[1]} alt="img" />
+                  <img src={event.photos[0]} alt="img" />{' '}
+                </div>
+              )}
 
-          <h2>Октябрь 2020</h2>
-        <ul className="events__list">
-          
-          <li className="events__item">
-            <img className="events__img" src={img04} alt="img"/>
-          </li>
-          <li className="events__item">
-            <img className="events__img" src={img05} alt="img"/>
-          </li>
-                  
-          </ul>
+              <div className="popup__close" onClick={() => setVisible(false)}>
+                +
+              </div>
+            </div>
+          </div>
+        )}
 
-
-
-
+        <button className="btn events__btn">Показать еще</button>
       </main>
     </>
   );
