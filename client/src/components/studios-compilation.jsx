c280d173f8bc3cfa8e2513b8a8bddbe2379a2d47
VasiliyMac <cartario@yandex.ru>
2020-12-10 09:30:38 +0300

    refactor compilation

---
+++ client/src/components/studios-compilation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+const Compilation = ({studios, title})=>{
+  const history = useHistory();
+
+  const handleClickCardCompilation = (id) => {
+    history.push(`/detail/${id}`);
+  }
+  return (
+    <div className="studios__compilation">
+          <h3>{title}</h3>
+          <ul className="studios__compilation-list">
+            {studios.map((studio)=><li key={studio.id} className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
+              <img src={studio.imgUrl} alt="imgStudio"/>
+              <p>{studio.title} </p>
+            </li>)
+            }
+          </ul>  
+        </div>
+  );
+};
+
+export default Compilation;
--- client/src/pages/studios.jsx
+++ client/src/pages/studios.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
 import Navbar from '../components/navbar';
 import Card from '../components/studioCard';
 import Header from '../components/header';
-import Footer from '../components/footer.jsx';
+import Compilation from '../components/studios-compilation';
 import {studios} from '../data/studios-mock';
 
 const SHOWING_BY_CLICK = 6;
 
 const Studios = () => {
   const [showingCards, setShowingCards] = React.useState(6);
-  const history = useHistory();
-
   const studiosCopy = [...studios].filter((studio)=>!studio.isDuplicate);
-
   const studiosKids = [...studios].filter((studio)=>!studio.isDuplicate&&studio.age_min<=6);
   const studiosDance = [...studios].filter((studio)=>!studio.isDuplicate&&(studio.type==='dance')||(studio.id==='15'));
   const studiosArt = [...studios].filter((studio)=>!studio.isDuplicate&&(studio.type==='art'));
@@ -25,95 +21,20 @@ const Studios = () => {
   const handleClickShowMore =()=>{
     setShowingCards((prev) => prev + SHOWING_BY_CLICK);
   }
-
-  const handleClickCardCompilation = (id) => {
-    history.push(`/detail/${id}`);
-  }
   
   return (
     <>      
       <Header title="Студии и секции"/>
       <main className="studios">         
-        <div className="container"> 
+        <div className="container">  
 
-        <div className="studios__compilation">
-          <h3>Для самых маленьких (до 6 лет):</h3>
-          <ul className="studios__compilation-list">
-            {studiosKids.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Музыка:</h3>
-          <ul className="studios__compilation-list">
-            {studiosMusic.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Танцы:</h3>
-          <ul className="studios__compilation-list">
-            {studiosDance.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Спорт:</h3>
-          <ul className="studios__compilation-list">
-            {studiosSport.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Порисовать:</h3>
-          <ul className="studios__compilation-list">
-            {studiosArt.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Тинэйджерам:</h3>
-          <ul className="studios__compilation-list">
-            {studiosTeenAge.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        <div className="studios__compilation">
-          <h3>Родителям:</h3>
-          <ul className="studios__compilation-list">
-            {studiosParents.map((studio)=><li className="studios__compilation-item" onClick={()=>handleClickCardCompilation(studio.id)}>
-              <img src={studio.imgUrl} alt="imgStudio"/>
-              <p>{studio.title} </p>
-            </li>)
-            }
-          </ul>  
-        </div>
-
-        
+        <Compilation studios={studiosKids} title={'Для самых маленьких (до 6 лет):'}/>
+        <Compilation studios={studiosMusic} title={'Музыка:'}/>
+        <Compilation studios={studiosDance} title={'Танцы:'}/>
+        <Compilation studios={studiosSport} title={'Спорт:'}/>
+        <Compilation studios={studiosArt} title={'Порисовать:'}/>
+        <Compilation studios={studiosTeenAge} title={'Тинэйджерам:'}/>
+        <Compilation studios={studiosParents} title={'Родителям:'}/>
 
         <h3>Все студии:</h3>
 
